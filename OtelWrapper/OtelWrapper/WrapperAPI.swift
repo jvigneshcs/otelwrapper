55458d25f3fed7f1a1b660f75225ff11db00dd82
J. Vignesh <vignesh.jeyaraj@hidglobal.com>
2023-01-09 17:44:39 +0530

    Error reproducible without Open telemetry

--- OtelWrapper/OtelWrapper/WrapperAPI.swift
+++ OtelWrapper/OtelWrapper/WrapperAPI.swift
@@ -9,16 +9,13 @@ import Foundation
 import NIOHTTP2
 import NIOPosix
 import GRPC
-import OpenTelemetryApi
-import OpenTelemetrySdk
-import OpenTelemetryProtocolExporter
 
 public typealias HTTPProtocol = HTTP2FramePayloadToHTTP1ClientCodec.HTTPProtocol
 
 @objc
 public class WrapperAPI: NSObject {
     
-    private let tracer: TracerSdk
+    private let client: ClientConnection
     
     @objc
     public convenience init(libraryName: String,
@@ -37,14 +34,13 @@ public class WrapperAPI: NSObject {
                   headers: headersTuples)
     }
     
-    public convenience init(libraryName: String,
+    public init(libraryName: String,
                             libraryVersion: String,
                             schema: HTTPProtocol = .https,
                             host: String,
                             port: Int = 443,
                             headers: [(String,String)]? = nil) {
         let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        let client: ClientConnection
         switch schema {
         case .https:
             client = ClientConnection.usingPlatformAppropriateTLS(for: eventLoopGroup)
@@ -59,53 +55,6 @@ public class WrapperAPI: NSObject {
                 )
             client = ClientConnection(configuration: configuration)
         }
-        self.init(libraryName: libraryName,
-                  libraryVersion: libraryVersion,
-                  client: client,
-                  headers: headers)
-    }
-    
-    public convenience init(libraryName: String,
-                            libraryVersion: String,
-                            client: ClientConnection,
-                            headers: [(String,String)]? = nil) {
-        let configuration = OtlpConfiguration(headers: headers)
-        self.init(libraryName: libraryName,
-                  libraryVersion: libraryVersion,
-                  client: client,
-                  configuration: configuration)
-    }
-    
-    private init(libraryName: String,
-                 libraryVersion: String,
-                 client: ClientConnection,
-                 configuration: OtlpConfiguration) {
-        tracer = OpenTelemetrySDK.instance
-            .tracerProvider
-            .get(instrumentationName: libraryName,
-                 instrumentationVersion: libraryVersion) as! TracerSdk
-        let traceExporter = OtlpTraceExporter(channel: client,
-                                              config: configuration)
-        let spanProcessor = SimpleSpanProcessor(spanExporter: traceExporter)
-        OpenTelemetrySDK.instance
-            .tracerProvider
-            .addSpanProcessor(spanProcessor)
         super.init()
-    }
-    
-    @objc
-    public func send(event: String,
-                     attributes: [String: Any],
-                     time: Date) {
-        let filtered = attributes.compactMapValues(AttributeValue.init)
-        let span = tracer.spanBuilder(spanName: event)
-            .setSpanKind(spanKind: .client)
-            .setStartTime(time: time)
-            .startSpan()
-        for (key, value) in filtered {
-            span.setAttribute(key: key,
-                              value: value)
-        }
-        span.end()
     }
 }
